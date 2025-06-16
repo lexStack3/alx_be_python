2847cff829cafe817d0fe0c713c6cd5414b26f1a
lexStack3 <alexanderedim80@gmail.com>
2025-06-16 18:50:04 +0100

    Basic arithmetic operations in Python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+number1 = 10
+number2 = 5
+print("Addition of {} and {} is {}".format(number1,
+                                           number2,
+                                           number1 + number2))
+print("Substraction of {} and {} is {}".format(number2,
+                                               number1,
+                                               number1 - number2))
+print("Multiplication of {} and {} is {}".format(number1,
+                                                 number2,
+                                                 number1 * number1))
