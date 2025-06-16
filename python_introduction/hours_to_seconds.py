6e805d8e7c8dc3445a8b4001c964e20fcfb4a828
lexStack3 <alexanderedim80@gmail.com>
2025-06-16 21:04:15 +0100

    Demonstrates understanding of variable assignments and arithmetic operations by converting a given number of hours into seconds.

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Demonstrate understanding of variable assignments and arithmetic
+operations by converting a given number of hours into seconds.
+"""
+hours = 2
+seconds = 3600 * hours
+print(f"{hours} hour(s) is {seconds} seconds.")
