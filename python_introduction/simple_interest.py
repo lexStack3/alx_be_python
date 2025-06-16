acc81a096924e369ef86522f147edeb0e8dbcd20
lexStack3 <alexanderedim80@gmail.com>
2025-06-16 20:52:02 +0100

    Applies basic arithmetic operations and variable assignments to calculate the simple interest on a given principle amoint, rate of interest, and time.

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,8 +3,8 @@
 assignments to calculate the simple interest on a given principal
 amount rate of interest, and time.
 """
-principle = 1000
+principal = 1000
 rate = 0.05
 time = 3
-interest = principle * rate * time
+interest = principal * rate * time
 print(f"The simple interest is: {interest}")
