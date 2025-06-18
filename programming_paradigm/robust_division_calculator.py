1e193efa4e16fb03b808715f0f033d43758b0c4f
lexStack3 <alexanderedim80@gmail.com>
2025-06-18 15:38:30 +0100

    Performs division and handle potential errors

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,6 +3,7 @@
 division by zero and non-numeric inputs using using command
 line arguments.
 """
+import sys
 
 def safe_divide(numerator, denominator):
     """Performs division and handle potential errors."""
@@ -11,9 +12,11 @@ def safe_divide(numerator, denominator):
         denominator = float(denominator)
     except ValueError:
         print("Error: Please enter numeric values only.")
+        sys.exit(1)
     else:
         try:
             result = numerator / denominator
             return f"The result of the division is {result:.1f}"
         except ZeroDivisionError:
             print("Error: Cannot divide by zero.")
+            sys.exit(1)
