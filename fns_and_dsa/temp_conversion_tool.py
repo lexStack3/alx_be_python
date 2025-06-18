036ada471ab1f650743ebdbcf5cd90fa6937f9c1
lexStack3 <alexanderedim80@gmail.com>
2025-06-18 12:35:56 +0100

    Defines functions to convert temperatures between Celsius and Fahrenheit.

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -4,8 +4,8 @@ converts temperatures between Celsius and Fahrenheit, using global
 variables to store conversion factors.
 """
 
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     """Converts fahrenheit to celsius."""
