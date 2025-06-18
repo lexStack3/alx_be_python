c2f419716687b77ed23f65c1c876899310f18711
lexStack3 <alexanderedim80@gmail.com>
2025-06-18 18:50:29 +0100

    Inheritance and Composition in Python

--- oop/library_system.py
+++ oop/library_system.py
@@ -20,7 +20,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"{self.__class__.__name__}: {self.title} by {self.author}, File Size: {self.file_size}"
+        return f"{self.__class__.__name__}: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
